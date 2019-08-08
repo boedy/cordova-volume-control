dc05e3d14d7906dd1ff0a03bdf6ba46efaa1cf54
Steven Roberts <sroberts@onlinedataintelligence.com>
2019-08-08 15:50:32 +0100

    Added getVolume()
    Added the option to get the current volume

--- src/ios/VolumeControl.h
+++ src/ios/VolumeControl.h
@@ -3,23 +3,22 @@
 
 @interface VolumeControl : CDVPlugin
 {
-    AVAudioSession* avSession;
+    AVAudioSession *avSession;
     UIView *volumeView;
-    NSString* callbackId;
+    NSString *callbackId;
     float launchVolume;
-    
 }
-@property (nonatomic, strong) AVAudioSession* avSession;
-@property (nonatomic, strong) CDVInvokedUrlCommand* callback;
-@property (nonatomic, retain) UIView *volumeView;
-@property (nonatomic) float launchVolume;
-@property (strong) NSString* callbackId;
-- (void) initCommand:(CDVInvokedUrlCommand*)command;
-- (void) destroyCommand:(CDVInvokedUrlCommand*)command;
-- (void) setVolumeCommand:(CDVInvokedUrlCommand*)command;
-- (void) showVolumeNotifications;
-- (void) hideVolumeNotifications;
-- (void) setVolume:(float)level;
+@property(nonatomic, strong) AVAudioSession *avSession;
+@property(nonatomic, strong) CDVInvokedUrlCommand *callback;
+@property(nonatomic, retain) UIView *volumeView;
+@property(nonatomic) float launchVolume;
+@property(strong) NSString *callbackId;
+- (void)initCommand:(CDVInvokedUrlCommand *)command;
+- (void)destroyCommand:(CDVInvokedUrlCommand *)command;
+- (void)setVolumeCommand:(CDVInvokedUrlCommand *)command;
+- (void)showVolumeNotifications;
+- (void)hideVolumeNotifications;
+- (void)setVolume:(float)level;
+- (void)getVolumeCommand:(CDVInvokedUrlCommand *)command;
 
 @end
-
